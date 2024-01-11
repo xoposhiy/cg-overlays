d91c67a564b26177857f2dbf882f0a1a5a98bd93
Pavel Egorov <xoposhiy@mail.ru>
2024-01-11 12:44:44 +0400

    Code Busters

--- extension/knownGames.js
+++ extension/knownGames.js
@@ -6,7 +6,8 @@ const viewports = {
     "FC2023" : "@vp -4600 -300 14600 10500",
     "Search Race": "@vp 0 0 16000 9000",
     "CodersStrikeBack": "@vp 0 0 16000 9000",
-    "CodinGame": "@vp 0 0 1920 1080"
+    "CodinGame": "@vp 0 0 1920 1080", // Code Royal ¯\_(ツ)_/¯
+    "CodeBusters": "@vp -350 -220 16350 9100"
 };
 
 const defaultViewport = "@vp 0 0 16000 9000";
